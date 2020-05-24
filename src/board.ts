3897529e6312c827385610d968013fb4699dc1e0
Aykut Kardaş <aykutkrds@gmail.com>
2020-05-25 00:39:56 +0300

    Board struct update. and and Positions added.

--- src/board.ts
+++ src/board.ts
@@ -8,32 +8,116 @@ export type ColumnType = {
   value: object;
 };
 
-export type RowType = {
-  [index: number]: ColumnType;
+export type RowType = ColumnType[];
+
+export type BoardType = RowType[];
+
+export type PositionsType = {
+  ET: string[];
+  EL: string[];
+  EB: string[];
+  ER: string[];
+  C: string[];
+  CTL: string[];
+  CTR: string[];
+  CBR: string[];
+  CBL: string[];
 };
 
 class Board {
   config: BoardConfig;
 
-  board: RowType[] = [];
+  board: BoardType = [];
 
-  constructor(config: BoardConfig) {
-    this.config = { ...config };
+  positions: PositionsType = {
+    ET: [],
+    EL: [],
+    EB: [],
+    ER: [],
+    C: [],
+    CTL: [],
+    CTR: [],
+    CBR: [],
+    CBL: [],
+  };
+
+  init(config: BoardConfig): Board {
+    const board = Board.createBoard(config);
+    const positions = Board.createPosition(board);
+
+    this.config = config;
+    this.board = board;
+    this.positions = positions;
+
+    return this;
   }
 
-  create(): RowType[] {
-    const { x, y } = this.config;
+  static createBoard = (config: BoardConfig): BoardType => {
+    const { x, y } = config;
+    const board: BoardType = [];
 
     for (let rowIndex = 0; rowIndex < x; rowIndex += 1) {
       const row = [];
       for (let columnIndex = 0; columnIndex < y; columnIndex += 1) {
         row.push({ id: `${rowIndex}_${columnIndex}`, value: null });
       }
-      this.board.push(row);
+      board.push(row);
     }
 
-    return this.board;
-  }
+    return board;
+  };
+
+  static createPosition = (board: BoardType): PositionsType => {
+    const positions: PositionsType = {
+      ET: [],
+      EL: [],
+      EB: [],
+      ER: [],
+      C: [],
+      CTL: [],
+      CTR: [],
+      CBR: [],
+      CBL: [],
+    };
+
+    // [TODO]: Find a way to write shorter.
+    board.forEach((row, rowId) => {
+      row.forEach((column, columnId) => {
+        const isFirstRow = rowId === 0;
+        const isFirstColumn = columnId === 0;
+        const isLastRow = rowId === board.length - 1;
+        const isLastColumn = columnId === row.length - 1;
+
+        if (isFirstRow) {
+          if (isFirstColumn) {
+            positions.CTL.push(column.id);
+          } else if (isLastColumn) {
+            positions.CTR.push(column.id);
+          } else if (isFirstRow) {
+            positions.ET.push(column.id);
+          }
+        } else if (!isFirstRow && !isLastRow) {
+          if (isFirstColumn) {
+            positions.EL.push(column.id);
+          } else if (isLastColumn) {
+            positions.ER.push(column.id);
+          } else {
+            positions.C.push(column.id);
+          }
+        } else if (isLastRow) {
+          if (isFirstColumn) {
+            positions.CBL.push(column.id);
+          } else if (isLastColumn) {
+            positions.CBR.push(column.id);
+          } else {
+            positions.EB.push(column.id);
+          }
+        }
+      });
+    });
+
+    return positions;
+  };
 }
 
 export default Board;
--- test/board.spec.ts
+++ test/board.spec.ts
@@ -5,7 +5,7 @@ import Board from '../src/board';
 
 describe('Board Tests', () => {
   it('Square Board 3x3', () => {
-    const board = new Board({ x: 3, y: 3 }).create();
+    const board = new Board().init({ x: 3, y: 3 }).board;
 
     expect(board).to.deep.equal([
       [{ id: '0_0', value: null }, { id: '0_1', value: null }, { id: '0_2', value: null }],
@@ -15,7 +15,7 @@ describe('Board Tests', () => {
   });
 
   it('Rectangle Board 3x5', () => {
-    const board = new Board({ x: 3, y: 5 }).create();
+    const board = new Board().init({ x: 3, y: 5 }).board;
 
     expect(board).to.deep.equal([
       [{ id: '0_0', value: null }, { id: '0_1', value: null }, { id: '0_2', value: null }, { id: '0_3', value: null }, { id: '0_4', value: null }],
@@ -25,7 +25,7 @@ describe('Board Tests', () => {
   });
 
   it('Rectangle Board 5x3', () => {
-    const board = new Board({ x: 5, y: 3 }).create();
+    const board = new Board().init({ x: 5, y: 3 }).board;
 
     expect(board).to.deep.equal([
       [{ id: '0_0', value: null }, { id: '0_1', value: null }, { id: '0_2', value: null }],
@@ -35,4 +35,21 @@ describe('Board Tests', () => {
       [{ id: '4_0', value: null }, { id: '4_1', value: null }, { id: '4_2', value: null }],
     ]);
   });
+
+  it('Positions on 3x3 Board', () => {
+    const positions = new Board().init({ x: 3, y: 3 }).positions;
+
+    expect(positions).to.deep.equal({
+      ET: ['0_1'],
+      EL: ['1_0'],
+      EB: ['2_1'],
+      ER: ['1_2'],
+      C: ['1_1'],
+      CTL: ['0_0'],
+      CTR: ['0_2'],
+      CBR: ['2_2'],
+      CBL: ['2_0']
+    });
+
+  })
 });
