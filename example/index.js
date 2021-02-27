d96ad96437a5c6b844ab5916153395d870696ccc
Aykut Kardaş <aykutkrds@gmail.com>
2021-02-27 17:52:08 +0300

    Update example.

--- example/index.js
+++ example/index.js
@@ -1,10 +1,23 @@
 const Board = require("../dist/board").default;
 const Rules = require("../dist/rules").default;
+const Item = require("../dist/item").default;
 
 const board = new Board().init({
-  x: 5,
-  y: 5,
+  x: 9,
+  y: 9,
 });
+
+const item = new Item({
+  rules: {
+    movement: {
+      angular: true,
+      linear: true,
+      stepCount: 4,
+    },
+  },
+});
+
+board.setItemToCoord("4|4", item);
 
 const rules = new Rules().init(board);
 
@@ -24,37 +37,72 @@ board.board.forEach((row) => {
     colElement.setAttribute("class", "column");
     colElement.setAttribute("data-coord", col.id);
 
-    const itemElement = document.createElement("div");
-    itemElement.setAttribute("class", "item");
-    itemElement.setAttribute("data-coord", col.id);
-    itemElement.innerHTML = col.id;
+    if (col.item) {
+      const itemElement = document.createElement("div");
+      itemElement.setAttribute("class", "item");
+      itemElement.setAttribute("data-coord", col.id);
+      // itemElement.innerHTML = item.name;
+      itemElement._props = item;
 
-    colElement.append(itemElement);
-    
+      colElement.append(itemElement);
+    }
+
     rowElement.append(colElement);
   });
 });
 
+function showAvailableCoords() {
+  const itemElement = document.querySelector(".item");
+  const colElements = document.querySelectorAll(".column");
 
-const colElements = document.querySelectorAll(".column");
+  for (colElement of colElements) {
+    colElement.classList.remove("available");
+  }
 
-for (colElement of colElements) {
-  colElement.addEventListener("mouseover", (e) => {
-    const coord = e.target.dataset.coord;
-    const availableColumns = rules.getAvaiblableColumn(coord);
+  const coord = itemElement.dataset.coord;
+  const availableColumns = rules.getAvaiblableColumn(
+    coord,
+    itemElement._props.rules
+  );
 
-    availableColumns.forEach((coord) => {
-      const availableColumn = document.querySelector(
-        '.column[data-coord="' + coord + '"]'
-      );
+  availableColumns.forEach((coord) => {
+    const availableColumn = document.querySelector(
+      '.column[data-coord="' + coord + '"]'
+    );
 
-      availableColumn.classList.add("available");
-    });
-  });
-
-  colElement.addEventListener("mouseout", () => {
-    for (colElement of colElements) {
-      colElement.classList.remove("available");
-    }
+    availableColumn.classList.add("available");
   });
 }
+
+function update() {
+  console.log(1);
+  const directionAngularEl = document.querySelector("#direction_angular");
+  const directionLinearEl = document.querySelector("#direction_linear");
+  const stepCountEl = document.querySelector("#step_count");
+
+  const isAngular = directionAngularEl.checked;
+  const isLinear = directionLinearEl.checked;
+  const stepCount = parseInt(stepCountEl.value);
+
+  const itemElement = document.querySelector(".item");
+
+  const item = new Item({
+    rules: {
+      movement: {
+        angular: isAngular,
+        linear: isLinear,
+        stepCount: stepCount,
+      },
+    },
+  });
+
+  itemElement._props = item;
+
+  showAvailableCoords();
+}
+
+document.querySelector("#direction_angular").addEventListener("change", update);
+document.querySelector("#direction_linear").addEventListener("change", update);
+document.querySelector("#step_count").addEventListener("change", update);
+
+window.update = update;
