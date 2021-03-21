eb81160c6dfe34befed76c421b94aaf828cba698
Aykut Kardaş <aykutkrds@gmail.com>
2021-03-21 03:23:45 +0300

    Refactor checkers-turkish Rules class.

--- src/rules/checkers-turkish.ts
+++ src/rules/checkers-turkish.ts
@@ -1,81 +1,33 @@
 import Board from "../board";
-import { CheckersItemType } from "../items/checkers-item";
-import useCoord from "../utils/useCoord";
+import Rules from "../rules/default-rules";
+import { MovementType } from "../items/default-item";
 
-class CheckersTurkishRules {
-  board: Board = null;
+class CheckersTurkishRules extends Rules {
+  rules: Rules;
 
   constructor(board: Board) {
-    this.board = board;
-    return this;
+    super(board);
+    this.rules = new Rules(board);
   }
 
-  getAvaiblableColumns = (id: string, item: CheckersItemType): string[] => {
-    const isExistCoord = this.board.isExistCoord(id);
-    if (!id || !item || !isExistCoord) {
-      return [];
-    }
-
-    const avaiblableColumns = [];
-    const [rowId, colId] = useCoord(id);
-    const { color, king } = item;
-    const stepCount = king ? 7 : 1;
-
-    if (color === "white") {
-      let fullColumnCount = 0;
-      for (let step = 1; step <= stepCount; step++) {
-        const columnCoord = `${rowId + step}|${colId}`;
-        const item = this.board.getItem(columnCoord);
-        if (item) {
-          fullColumnCount++;
-        }
-
-        if (fullColumnCount > 1) {
-          fullColumnCount = 0;
-          break;
-        }
-
-        if (item && item.color === "black") {
-          const nextColumnCoord = `${rowId + (step + 1)}|${colId}`;
-          const nextColumnIsEmpty = this.board.isEmpty(nextColumnCoord);
-
-          if (nextColumnIsEmpty) {
-            avaiblableColumns.push(nextColumnCoord);
-          }
+  getAvailableColumns = (id: string, movement: MovementType) => {
+    const avaiblableColumns = this.rules.getAvaiblableColumns(id, movement);
+    return avaiblableColumns
+      .map((coord) => {
+        if (this.board.isEmpty(coord)) {
+          return coord;
         } else {
-          avaiblableColumns.push(columnCoord);
-        }
-      }
-    } else if (color === "black") {
-      let fullColumnCount = 0;
+          const direction = this.board.getDirection(id, coord);
+          const movement = { stepCount: 1 };
 
-      for (let step = 1; step <= stepCount; step++) {
-        const columnCoord = `${rowId - step}|${colId}`;
-        const item = this.board.getItem(columnCoord);
-        if (item) {
-          fullColumnCount++;
-        }
+          movement[direction] = true;
 
-        if (fullColumnCount > 1) {
-          fullColumnCount = 0;
-          break;
-        }
-        if (item && item.color === "white") {
-          const nextColumnCoord = `${rowId - (step + 1)}|${colId}`;
-          const nextColumnIsEmpty = this.board.isEmpty(nextColumnCoord);
+          const [toCoord] = this.rules.getAvaiblableColumns(coord, movement);
 
-          if (nextColumnIsEmpty) {
-            avaiblableColumns.push(nextColumnCoord);
-          }
-        } else {
-          avaiblableColumns.push(columnCoord);
+          return toCoord;
         }
-      }
-    }
-
-    return avaiblableColumns.filter(
-      (coord) => this.board.isExistCoord(coord) && this.board.isEmpty(coord)
-    );
+      })
+      .filter((coord) => !!coord && this.board.isEmpty(coord));
   };
 }
 
