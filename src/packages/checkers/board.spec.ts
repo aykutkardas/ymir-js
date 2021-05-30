c18e9fb5a4fd0b5a8ae13634a2e9391a500e6bd4
ofsahof <ofsahof@gmail.com>
2021-05-30 19:34:17 +0300

    test:[#6] default checkers board config tests
    Signed-off-by: ofsahof <ofsahof@gmail.com>

---
+++ src/packages/checkers/board.spec.ts
@@ -0,0 +1,15 @@
+import { expect } from "chai";
+import "mocha";
+
+import CheckersBoard from "./board";
+import Item from "./item";
+
+describe("Checkers Board Tests",() => {
+	it("Default Checkers Board",() => {
+		const board = new CheckersBoard();
+
+		expect(board.config).to.deep.equal({
+			x: 8, y: 8
+		});
+	});
+});
